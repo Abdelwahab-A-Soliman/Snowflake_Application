fa42e62fa4669b464f17441fcd40eff8d700cb0c
Abdelwahab Soliman <64665756+Abdelwahab-A-Soliman@users.noreply.github.com>
2023-02-26 16:13:44 +0200

    Update Streamlit_app.py
    Removing "streamlit.text(fruityvice_response.json()) "

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -26,6 +26,5 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) 
 fruityvice_table = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_table)
